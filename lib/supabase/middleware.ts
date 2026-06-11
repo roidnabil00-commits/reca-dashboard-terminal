e5d89f4fb443d1aa33bce2df4a06a7d70ca25fb9
roidnabil00 <roidnabil00@gmail.com>
2026-06-11 10:07:07 +0700

    supabse

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -12,7 +12,8 @@ export async function updateSession(request: NextRequest) {
         getAll() {
           return request.cookies.getAll()
         },
-        setAll(cookiesToSet) {
+        // Tambahkan : any[] di sebelah cookiesToSet
+        setAll(cookiesToSet: any[]) {
           cookiesToSet.forEach(({ name, value }) =>
             request.cookies.set(name, value)
           )
